61036e2a458d934bc0f44eda1ec0b44ad03237b0
Gareth Coles <gdude2002@gmail.com>
2020-12-13 14:38:20 +0000

    1.1: Update to Kotlin 1.4

--- src/main/kotlin/com/kotlindiscord/api/client/serialisers/InfractionTypeSerialiser.kt
+++ src/main/kotlin/com/kotlindiscord/api/client/serialisers/InfractionTypeSerialiser.kt
@@ -2,12 +2,19 @@ package com.kotlindiscord.api.client.serialisers
 
 import com.kotlindiscord.api.client.enums.InfractionType
 import com.kotlindiscord.api.client.enums.toInfractionType
-import kotlinx.serialization.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
 
 @Serializer(forClass = Instant::class)
 class InfractionTypeSerialiser : KSerializer<InfractionType> {
-    override val descriptor: SerialDescriptor = PrimitiveDescriptor("WithCustomDefault", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("WithCustomDefault", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: InfractionType) {
         encoder.encodeString(value.type)
--- src/main/kotlin/com/kotlindiscord/api/client/serialisers/LocalDateTimeSerialiser.kt
+++ src/main/kotlin/com/kotlindiscord/api/client/serialisers/LocalDateTimeSerialiser.kt
@@ -1,6 +1,12 @@
 package com.kotlindiscord.api.client.serialisers
 
-import kotlinx.serialization.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -9,7 +15,8 @@ import java.time.format.DateTimeFormatter
 class LocalDateTimeSerialiser : KSerializer<LocalDateTime> {
     private val df: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy  HH:mm:ss.SSS")
 
-    override val descriptor: SerialDescriptor = PrimitiveDescriptor("WithCustomDefault", PrimitiveKind.STRING)
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("WithCustomDefault", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: LocalDateTime) = encoder.encodeString(value.format(df))
 
